e97c64e28c43345c5c90b1c41f0e1545a2a535d9
Roukoumanou <roukoumanouamidou@gmail.com>
2022-07-04 11:13:38 +0200

    Feature: Migration symfony 3.1 from 6.1

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login", methods={"GET", "POST"})
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return void
+     */
+    public function loginAction(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        // This code is never executed.
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserController extends AbstractController
+{
+    private EntityManagerInterface $em;
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $hasher)
+    {
+        $this->em = $em;
+        $this->hasher = $hasher;
+    }
+
+    /**
+     * @Route("/users", name="user_list")
+     */
+    public function listAction(UserRepository $userRepository)
+    {
+        return $this->render('user/list.html.twig', ['users' => $userRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/users/create", name="user_create", methods={"GET", "POST"})
+     */
+    public function createAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $this->hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/users/{id}/edit", name="user_edit")
+     */
+    public function editAction(User $user, Request $request)
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $this->hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $this->em->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été modifié");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+    }
+}
